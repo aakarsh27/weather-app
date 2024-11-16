75f8df1eded0302ff080186b2a429c0cdf7d2cbc
Aakarsh Sharma <aakarshjai@gmail.com>
2024-11-16 15:39:16 +0530

    importing all icos

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,12 @@
 import './Weather.css'
 import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import humidity_icon from '../assets/humidity.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
 
 const Weather = () => {
   return (
@@ -8,6 +15,7 @@ const Weather = () => {
             <input type="text" placeholder='Search...' />
             <img src={search_icon} alt=''/>
         </div>
+        <img src='' alt=''/>
     </div>
   )
 }
